43d411f4cc2bcc13bf5ef78447c1378cc8744b43
antarikshkudal10 <46812262+antarikshkudal10@users.noreply.github.com>
2019-04-17 10:47:21 +0530

    Add files via upload

---
+++ try1.m
@@ -0,0 +1,6 @@
+clc;clear all;close all;
+i=[45,85,12,16,72,46,76,32,95,48,87,74,96,45,84,67,97,35,71,64,72];
+for j=1:21
+    thingSpeakWrite(749348,i(j),'WriteKey','U0BBH913YW29S2P3')         
+    pause(15)
+end
